e84f9c17c961cfd0175802ec84deec50e1dc8ff7
kelderpassos <kelder.passos@gmail.com>
2025-01-21 10:24:43 -0300

    chore: remove linhas comentadas

--- aws-wordpress-website/modules/wordpress/userdata.sh
+++ aws-wordpress-website/modules/wordpress/userdata.sh
@@ -17,11 +17,3 @@ sudo cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 # executa wordpress sobre com apache como usuario
 sudo chown -R apache:apache /var/www/html/
 sudo rm -rf wordpress lastest.tar.gz
-
-# mysql -h DNS DO RDS AQUI -P 3306 -u admin -p
-# CREATE USER 'wordpress' IDENTIFIED BY 'SENHA AQUI'; GRANT ALL PRIVILEGES ON BigtradeMainSiteDb.* TO 'wordpress'; FLUSH PRIVILEGES; EXIT;
-# cd /var/www/html
-# sudo vi wp-config.php
-# db_name NOME DO BANCO RDS db_user USUARIO DO RDS db_password SENHA DO RDS db_host PONTO DE ACESSO DO RDS
-# curl -s https://api.wordpress.org/secret-key/1.1/salt/
-# sudo systemctl restart httpd.service
